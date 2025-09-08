055e2baa8ef4963f217eca0981c89137ac6be36d
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-07 23:56:21 -0500

    prograa 3, de circulo

--- actividad3_circulo/Program.cs
+++ actividad3_circulo/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+//circulos
 Console.WriteLine("Bienvenido, calculemos círculos!");
 
 var radio = 0d; //declaracion de variable
