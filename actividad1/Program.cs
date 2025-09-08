5043d972690382ae77ba45a61838af326185a10c
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-07 22:51:39 -0500

    programa 1 listo

--- actividad1/Program.cs
+++ actividad1/Program.cs
@@ -1,4 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Este ejemplo es de git");
 
-//haré cambios en este programa
+//Actividad 1
+//programa que calcula el area de un rectangulo
+double ladoA;
+double ladoB;
+double resultado;
+
+Console.WriteLine("Calcula el area de un rectangulo!!");
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+
+resultado = ladoA * ladoB;
+
+Console.WriteLine("El lado A es: " + ladoA + " El lado B es: " + ladoB + " El resultado es: " + resultado);
