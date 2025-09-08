a635d30e978e3c50d742bf90d970a0cbfc630052
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-07 23:55:19 -0500

    programa 3, terminado

---
+++ actividad3_circulo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Bienvenido, calculemos círculos!");
+
+var radio = 0d; //declaracion de variable
+var resultado = 0d;
+
+const double Pi =  3.14;
+
+Console.WriteLine("Ingrese el valor del radio: ");//entrada de datos
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Pi * radio * radio;//operacion
+
+Console.WriteLine("El área del círculo es: " + resultado);//salida de datos
+
