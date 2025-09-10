f42bdcfb89f550a557472c58180cb0ea7f38fb9d
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-09 22:31:11 -0500

    Actividad 5, operadores relacionales

---
+++ Actividad5/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//declaración de variales implícitas
+var (valor1, valor2, valor3) = (10, 9, 9);
+
+//salida de estos datos
+Console.WriteLine("Los valores son: " + valor1 + ", " + valor2 + ", " + valor3);
+
+bool result = valor1 == valor2;
+Console.WriteLine("El resultado de la comparación de valor1 == valor2 es: " + result);
+
+result = valor1 != valor3;
+Console.WriteLine("El resultado de la comparación de valor1 != valor2 es: " + result);
+
+result = valor2 > valor3;
+Console.WriteLine("El resultado de la comparación de valor2 > valor3 es: " + result);
+
+result = valor2 < valor3;
+Console.WriteLine("El resultado de la comparación de valor2 < valor3 es: " + result);
+
+result = valor1 >= valor2;
+Console.WriteLine("El resultado de la comparación de valor1 >= valor2 es: " + result);
+
+result = valor3 <= valor1;
+Console.WriteLine("El resultado de la comparación valor3 <= valor1 es: " + result);
+
