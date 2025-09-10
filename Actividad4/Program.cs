89341f4a02e35f65e11a4d977c8ebf5ae55db70e
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-09 22:20:43 -0500

    operadores, actividad 4

--- Actividad4/Program.cs
+++ Actividad4/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+//declaración de variables
+bool valor1 = true;
+bool valor2 = true;    
+bool valor3 = false;
+
+//declaración implícita var (valor1, valor2, valor3) = (true, true, false);
+
+// AND lógico
+bool resultAnd = valor1 && valor2 && valor3;
+Console.WriteLine(resultAnd);
+
+// OR lógico
+bool resultOr = valor1 || valor2 || valor3;
+Console.WriteLine(resultOr);
+
+// AND + OR combinado
+bool resultAndOr = (valor1 && valor2) || valor3;
+Console.WriteLine(resultAndOr);
+
+// NOT lógico
+bool resultNot = !valor1;
+Console.WriteLine(resultNot);
+
+// XOR lógico
+bool resultXor = (valor1 ^ valor2) ^ valor3;
+Console.WriteLine(resultXor);
+
