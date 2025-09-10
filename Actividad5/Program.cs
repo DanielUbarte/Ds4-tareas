c3254f395fd73a533b315870d7c4a979277ca058
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-10 11:13:14 -0500

    actividad 5, documentacion

--- Actividad5/Program.cs
+++ Actividad5/Program.cs
@@ -6,21 +6,27 @@ var (valor1, valor2, valor3) = (10, 9, 9);
 //salida de estos datos
 Console.WriteLine("Los valores son: " + valor1 + ", " + valor2 + ", " + valor3);
 
+//comparaciones con operador igual
 bool result = valor1 == valor2;
 Console.WriteLine("El resultado de la comparación de valor1 == valor2 es: " + result);
 
+//comparación con operador diferente
 result = valor1 != valor3;
 Console.WriteLine("El resultado de la comparación de valor1 != valor2 es: " + result);
 
+//comparaciones con operador mayor que
 result = valor2 > valor3;
 Console.WriteLine("El resultado de la comparación de valor2 > valor3 es: " + result);
 
+//comparaciones con operador menor que
 result = valor2 < valor3;
 Console.WriteLine("El resultado de la comparación de valor2 < valor3 es: " + result);
 
+//comparaciones con operador mayor o igual que
 result = valor1 >= valor2;
 Console.WriteLine("El resultado de la comparación de valor1 >= valor2 es: " + result);
 
+//comparaciones con operador menor o igual que
 result = valor3 <= valor1;
 Console.WriteLine("El resultado de la comparación valor3 <= valor1 es: " + result);
 
