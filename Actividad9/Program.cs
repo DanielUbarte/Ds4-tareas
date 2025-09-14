551ef5c2556d13af7164620c860573e8dd00bb49
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-13 23:06:38 -0500

    Actividad 9, ciclo while

--- Actividad9/Program.cs
+++ Actividad9/Program.cs
@@ -53,7 +53,7 @@ while (true) //inicia el ciclo
             }
             break;
             default: //si la opción no es ni reglas ni 21
-            Console.WriteLine("Opción inválida, escribe 'reglas' o '21'");
+            Console.WriteLine("Opción inválida, escribe 'reglas' o '21'"); //mensaje de opción inválida
             break;
     }
 }
