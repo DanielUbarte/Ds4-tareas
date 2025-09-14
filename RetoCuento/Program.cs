97a47bf473c2bd3081ce19e22f657f475f209124
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-13 23:32:23 -0500

    RetoCuento

---
+++ RetoCuento/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+// 1) Leer cuento.txt
+if (!File.Exists("cuento.txt"))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("No se encontró 'cuento.txt' en el directorio actual.");
+    Console.ResetColor();
+    Console.WriteLine("Coloca el archivo junto al ejecutable o márcalo como 'Copy to Output Directory'.");
+    return;
+}
+
+// ===== RETO 1: string.Concat() =====
+{
+    string parte1 = "Bitín";
+    string parte2 = "Explorador";
+    string salida = string.Concat(parte1, " ", parte2);
+    Console.WriteLine("nos muestra utilizando el concat, lo siguiente: "+ salida);
+}
