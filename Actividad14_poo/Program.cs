aad46e588d23fd65c3d3d7d57cd02c6a0f638788
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-21 21:46:26 -0500

    creación de la clase super heroe

---
+++ Actividad14_poo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+var superman = new SuperHero();
+
+superman.ID = 1;
+superman.Name = "Superman";
+superman.IdentidadSecreta = "Clark Kent";
+superman.Ciudad = "Metropolis";
+superman.SuperPoderes = new [] { "Super Fuerza", "Vuelo", "Visión de Rayos X" };
+superman.PuedeVolar = true;
+
+class SuperHero {
+    public int ID;
+    public string Name;
+    public string IdentidadSecreta;
+    public string Ciudad;
+    public string[] SuperPoderes;
+    public bool PuedeVolar;
+
+}
