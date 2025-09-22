6ada3c7869aaf236fab74456bb08f68a2ebc6c6b
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-22 10:59:42 -0500

    documentacion en clases

--- Actividad19_poo/Models/SuperHeroe.cs
+++ Actividad19_poo/Models/SuperHeroe.cs
@@ -1,34 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;                           // Espacio de nombres base de .NET
+using System.Collections.Generic;       // Para usar List<T>
+using System.Linq;                      // Métodos de LINQ (no usados aquí, pero referenciados)
+using System.Text;                      // Para usar StringBuilder
+using System.Threading.Tasks;           // Para programación asíncrona (no usado aquí)
 
-namespace Actividad19_poo.Models
+namespace Actividad19_poo.Models         // Namespace donde está contenida la clase
 {
+    // Clase que representa a un Superhéroe
     class SuperHero
     {
-        public int ID;
-        public string Nombre;
-        public string IdentidadSecreta;
-        public string Ciudad;
-        public List<SuperPoder> SuperPoderes;
-        public bool PuedeVolar;
+        public int ID;                   // Identificador único del héroe
+        public string Nombre;            // Nombre del superhéroe (ej: "Superman")
+        public string IdentidadSecreta;  // Identidad secreta (ej: "Clark Kent")
+        public string Ciudad;            // Ciudad donde actúa el héroe
+        public List<SuperPoder> SuperPoderes; // Lista de poderes asociados al héroe
+        public bool PuedeVolar;          // Indica si el héroe tiene la habilidad de volar
 
+        // Constructor por defecto
         public SuperHero()
         {
-            ID = 1;
-            SuperPoderes = new List<SuperPoder>();
-            PuedeVolar = false;
+            ID = 1;                      // Valor inicial del ID
+            SuperPoderes = new List<SuperPoder>(); // Inicializa la lista de poderes vacía
+            PuedeVolar = false;          // Por defecto, se asume que no puede volar
         }
+
+        // Método que permite al héroe "usar" sus superpoderes
         public string UsarSuperPoderes()
         {
-            StringBuilder sb = new StringBuilder();
-            foreach (var item in SuperPoderes)
+            StringBuilder sb = new StringBuilder(); // Objeto para construir cadenas dinámicamente
+            foreach (var item in SuperPoderes)      // Recorre cada superpoder asignado
             {
+                // Agrega una línea de texto indicando que está usando dicho poder
                 sb.AppendLine($"{Nombre} esta usando el super poder {item.Nombre}!!");
             }
-            return sb.ToString();
+            return sb.ToString();       // Devuelve el listado de frases en un solo string
         }
     }
 }
--- Actividad19_poo/Models/SuperPoder.cs
+++ Actividad19_poo/Models/SuperPoder.cs
@@ -1,19 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;                          // Espacio de nombres base de .NET
+using System.Collections.Generic;      // Colecciones genéricas (List, Dictionary, etc.)
+using System.Linq;                     // Métodos LINQ para colecciones
+using System.Text;                     // Funcionalidades para manejo de texto y codificaciones
+using System.Threading.Tasks;          // Soporte para tareas asíncronas
 
-namespace Actividad19_poo.Models
+namespace Actividad19_poo.Models        // Namespace que organiza las clases del proyecto
 {
+    // Clase que representa un superpoder de un superhéroe
     class SuperPoder
     {
-        public string Nombre;
-        public string decripcion;
-        public NivelPoder Nivel;
+        public string Nombre;           // Nombre del poder (ej: "Super Fuerza", "Volar")
+        public string decripcion;       // Descripción del poder (ej: "Capacidad para volar")
+        public NivelPoder Nivel;        // Nivel del poder (enum definido en otro archivo)
 
+        // Constructor por defecto
         public SuperPoder()
         {
+            // Se asigna como nivel inicial 'niveluno' al crear cualquier superpoder
             Nivel = NivelPoder.niveluno;
         }
     }
