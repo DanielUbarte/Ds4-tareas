33a6209e994efbbb5fceb89a729f04181043a1d2
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-21 21:01:18 -0500

    Video 4, crear clase y ver objetos

--- Actividad13_poo/Program.cs
+++ Actividad13_poo/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+Apuntador apuntadorPlatzi = new Apuntador();
+Apuntador apuntadorPlatzi2 = new Apuntador();
 
+apuntadorPlatzi.color = "Negro";
+Console.WriteLine(apuntadorPlatzi.color);
+apuntadorPlatzi2.color = "Azul";
+Console.WriteLine(apuntadorPlatzi2.color);
+
+class Apuntador {
+    public string color;
+    public double largo;
+    public short NumeroDeBotones;
+    public bool Stikcers;
+}
