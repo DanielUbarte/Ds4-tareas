3f0ab599cc86fa50246d9ca02cd523458c4e688b
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-22 11:25:35 -0500

    video 4, terminado

--- Actividad13_poo/Program.cs
+++ Actividad13_poo/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
+//video 4
+
 Apuntador apuntadorPlatzi = new Apuntador();
 Apuntador apuntadorPlatzi2 = new Apuntador();
 
