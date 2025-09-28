d7ae1a862d525c1ffec52253574692ae9df51666
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-09-27 20:55:44 -0500

    Video 14, terminado, herencia

---
+++ Actividad21_poo/model/AntiHeroe.cs
@@ -0,0 +1,17 @@
+﻿using Actividad19_poo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad21_poo.model
+{
+    internal class AntiHeroe : SuperHeroe
+    {
+        public string RealizarAccionDeAntiheroe(string accion)
+        { 
+            return $"El Antiheroe {NombreEIdentidadSecreta} ha realizado {accion}";
+        }
+    }
+}
