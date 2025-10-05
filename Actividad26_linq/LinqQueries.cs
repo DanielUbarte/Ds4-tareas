3fcc8e57e073267f9785a38e0b9238962eb02608
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-10-05 16:21:17 -0500

    terminación del video 7, configuración para utilización

---
+++ Actividad26_linq/LinqQueries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad26_linq
+{
+    public class LinqQueries
+    {
+        public LinqQueries()
+        { 
+        
+        }
+    }
+}
